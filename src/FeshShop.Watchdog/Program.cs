56b7ac4d682c22aca825a9f175cdc43363590ee2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-13 15:32:45 +0300

    configure the project, add Identity service for health checks

---
+++ src/FeshShop.Watchdog/Program.cs
@@ -0,0 +1,14 @@
+namespace FeshShop.Watchdog
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args) 
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(wb => wb.UseStartup<Startup>())
+                .Build()
+                .Run();
+    }
+}
